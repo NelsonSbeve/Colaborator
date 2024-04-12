e16dac7c447e234ea8d984bda1dde6be42e48a51
Nelson Aguiar <nelson.aguiar@b-simple.pt>
2024-04-12 14:24:07 +0100

    new new

--- Application/Services/ColaboratorConsumer.cs
+++ Application/Services/ColaboratorConsumer.cs
@@ -1,5 +1,6 @@
 using Application.Services;
 using Domain.Model;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -13,37 +14,33 @@ public class ColaboratorConsumer : IColaboratorConsumer
         private readonly ConnectionFactory _factory;
         private readonly IConnection _connection;
         private readonly IModel _channel;
+        private string queueName;
  
         public ColaboratorConsumer()
         {
-            _factory = new ConnectionFactory { HostName = "localhost" };
-            _connection = _factory.CreateConnection();
-            _channel = _connection.CreateModel();
- 
+            var factory = new ConnectionFactory { HostName = "localhost" };
+             _connection = factory.CreateConnection();
+             _channel = _connection.CreateModel();
+
             _channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
 
-            _channel.QueueBind(queue: "collaborator_queue",
-                  exchange: "logs",
-                  routingKey: string.Empty);
- 
-            Console.WriteLine(" [*] Waiting for messages.");
+            queueName = _channel.QueueDeclare().QueueName;
+
+            _channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+            Console.WriteLine(" [*] Waiting for logs.");
         }
  
         public void StartConsuming()
         {        
-            var consumer = new EventingBasicConsumer(_channel);
+           var consumer = new EventingBasicConsumer(_channel);
             consumer.Received += (model, ea) =>
             {
-                var body = ea.Body.ToArray();
+                byte[] body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
- 
-                Console.WriteLine($" [x] Received {message}");
- 
-                
+                Console.WriteLine($" [x] {message}");
             };
-            _channel.BasicConsume(queue: "collaborator_queue",
-                                autoAck: true,
-                                consumer: consumer);
+
+            _channel.BasicConsume(queue:queueName, autoAck: true, consumer: consumer);
         }
 }
 
